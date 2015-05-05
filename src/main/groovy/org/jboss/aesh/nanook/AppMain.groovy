e21441df4a0650948a58de73f5b56912b1b1a060
eprogramming <00hf11@gmail.com>
2015-05-05 20:46:00 -0300

    #116

--- src/main/groovy/org/jboss/aesh/nanook/AppMain.groovy
+++ src/main/groovy/org/jboss/aesh/nanook/AppMain.groovy
@@ -3,73 +3,73 @@
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the 'License') you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
- * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.nanook;
+package org.jboss.aesh.nanook
 
-import org.esmerilprogramming.overtown.server.Overtown;
-import org.esmerilprogramming.overtown.server.ConfigurationBuilder;
+import org.esmerilprogramming.overtown.server.Overtown
+import org.esmerilprogramming.overtown.server.ConfigurationBuilder
 
 /**
- * @author <a href="mailto:00hf11@gmail.com">Helio Frota</a>
+ * @author <a href='mailto:00hf11@gmail.com'>Helio Frota</a>
  */
 class AppMain {
 
-  private static final String HELP = "usage: java -jar -h 192.168.1.1 -p 8080 -c app";
+  private static final String HELP = 'usage: java -jar -h 192.168.1.1 -p 8080 -c app'
 
   static main(String... args) {
 
-    ConfigurationBuilder cb = new ConfigurationBuilder();
-    cb.withHost("0.0.0.0");
-    cb.withAppContext("app");
-    cb.withPackageToScan("org.jboss.aesh.nanook");
+    ConfigurationBuilder cb = new ConfigurationBuilder()
+    cb.withHost('0.0.0.0')
+    cb.withAppContext('app')
+    cb.withPackageToScan('org.jboss.aesh.nanook')
 
-    List<String> options = new ArrayList<>();
+    List<String> options = new ArrayList<>()
     for (String o : args) {
-      options.add(o);
+      options.add(o)
     }
 
-    if (options.contains("help")) {
-      System.out.println(HELP);
-      return;
+    if (options.contains('help')) {
+      println(HELP)
+      return
     }
 
     if (options.size() % 2 != 0) {
-      System.out.println(HELP);
-      return;
+      println(HELP)
+      return
     }
 
-    if (options.contains("-h")) {
-      String host = options.get(options.indexOf("-h") + 1);
+    if (options.contains('-h')) {
+      String host = options.get(options.indexOf('-h') + 1)
       if (!host.trim().isEmpty()) {
-        cb.withHost(host);
+        cb.withHost(host)
       }
     }
 
-    if (options.contains("-p")) {
-      String port = options.get(options.indexOf("-p") + 1);
+    if (options.contains('-p')) {
+      String port = options.get(options.indexOf('-p') + 1)
       if (!port.trim().isEmpty()) {
         for (char c : port.toCharArray()) {
           if (!Character.isDigit(c)) {
-            return;
+            return
           }
         }
-        cb.withPort(Integer.parseInt(port));
+        cb.withPort(Integer.parseInt(port))
       }
     }
 
-    if (options.contains("-c")) {
-      String context = options.get(options.indexOf("-c") + 1);
+    if (options.contains('-c')) {
+      String context = options.get(options.indexOf('-c') + 1)
       if (!context.trim().isEmpty()) {
-        cb.withAppContext(context);
+        cb.withAppContext(context)
       }
     }
 
-    new Overtown(cb.build());
+    new Overtown(cb.build())
   }
 }
--- src/main/groovy/org/jboss/aesh/nanook/controller/AeshHandler.groovy
+++ src/main/groovy/org/jboss/aesh/nanook/controller/AeshHandler.groovy
@@ -3,44 +3,37 @@
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the "License") you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
  * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.nanook.controller;
+package org.jboss.aesh.nanook.controller
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PrintStream;
-import java.util.Set;
-
-import org.jboss.aesh.console.AeshConsole;
-import org.jboss.aesh.console.AeshConsoleBuilder;
-import org.jboss.aesh.console.Config;
-import org.jboss.aesh.console.command.Command;
-import org.jboss.aesh.console.command.registry.AeshCommandRegistryBuilder;
-import org.jboss.aesh.console.command.registry.CommandRegistry;
-import org.jboss.aesh.console.command.registry.MutableCommandRegistry;
-import org.jboss.aesh.console.settings.Settings;
-import org.jboss.aesh.console.settings.SettingsBuilder;
-import org.jboss.aesh.extensions.cat.Cat;
-import org.jboss.aesh.extensions.cd.Cd;
-import org.jboss.aesh.extensions.clear.Clear;
-import org.jboss.aesh.extensions.echo.Echo;
-import org.jboss.aesh.extensions.exit.Exit;
-import org.jboss.aesh.extensions.ls.Ls;
-import org.jboss.aesh.extensions.mkdir.Mkdir;
-import org.jboss.aesh.extensions.pwd.Pwd;
-import org.jboss.aesh.extensions.rm.Rm;
-import org.jboss.aesh.extensions.mv.Mv;
-import org.jboss.aesh.extensions.touch.Touch;
-import org.jboss.aesh.parser.Parser;
-import org.jboss.logging.Logger;
+import org.jboss.aesh.console.AeshConsole
+import org.jboss.aesh.console.AeshConsoleBuilder
+import org.jboss.aesh.console.Config
+import org.jboss.aesh.console.command.Command
+import org.jboss.aesh.console.command.registry.AeshCommandRegistryBuilder
+import org.jboss.aesh.console.command.registry.CommandRegistry
+import org.jboss.aesh.console.command.registry.MutableCommandRegistry
+import org.jboss.aesh.console.settings.Settings
+import org.jboss.aesh.console.settings.SettingsBuilder
+import org.jboss.aesh.extensions.cat.Cat
+import org.jboss.aesh.extensions.cd.Cd
+import org.jboss.aesh.extensions.clear.Clear
+import org.jboss.aesh.extensions.echo.Echo
+import org.jboss.aesh.extensions.exit.Exit
+import org.jboss.aesh.extensions.ls.Ls
+import org.jboss.aesh.extensions.mkdir.Mkdir
+import org.jboss.aesh.extensions.pwd.Pwd
+import org.jboss.aesh.extensions.rm.Rm
+import org.jboss.aesh.extensions.mv.Mv
+import org.jboss.aesh.extensions.touch.Touch
+import org.jboss.aesh.parser.Parser
+import org.jboss.logging.Logger
 
 /**
  * @author Efraim Gentil
@@ -48,141 +41,131 @@ import org.jboss.logging.Logger;
  */
 public class AeshHandler {
 
-    private static final Logger LOGGER = Logger.getLogger(AeshHandler.class);
+  private static final Logger LOGGER = Logger.getLogger(AeshHandler)
 
-    private PipedOutputStream pos;
-    private PipedInputStream pis;
-    private ByteArrayOutputStream stream;
-    private Settings settings;
-    private AeshConsole aeshConsole;
-    private CommandRegistry registry;
-    private boolean debugMode;
+  private PipedOutputStream pos
+  private PipedInputStream pis
+  private ByteArrayOutputStream stream
+  private Settings settings
+  private AeshConsole aeshConsole
+  private CommandRegistry registry
+  private boolean debugMode
 
-    @SuppressWarnings("unchecked")
-    public AeshHandler(boolean debugMode) {
+  @SuppressWarnings("unchecked")
+  AeshHandler(boolean debugMode) {
 
-        this.debugMode = debugMode;
+    this.debugMode = debugMode
+    pos = new PipedOutputStream()
+    try {
+      pis = new PipedInputStream(pos)
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage())
+    }
+    stream = new ByteArrayOutputStream()
 
-        pos = new PipedOutputStream();
-        try {
-            pis = new PipedInputStream(pos);
-        }
-        catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-        stream = new ByteArrayOutputStream();
+    settings =
+      new SettingsBuilder().inputStream(pis)
+      .outputStreamError(new PrintStream(stream))
+      .outputStream(new PrintStream(stream))
+      .create()
 
-        settings =
-            new SettingsBuilder().inputStream(pis)
-                .outputStreamError(new PrintStream(stream))
-                .outputStream(new PrintStream(stream))
-                .create();
-
-        try {
-            add(Cd.class, Ls.class, Mkdir.class, Pwd.class, Rm.class, Mv.class, Touch.class, Cat.class,
-                Clear.class, Echo.class, Exit.class);
-        }
-        catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-
+    try {
+      add(Cd, Ls, Mkdir, Pwd, Rm, Mv, Touch, Cat, Clear, Echo, Exit)
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage())
     }
 
-    public AeshHandler() {
-        this(false);
+  }
+
+  AeshHandler() {
+    this(false)
+  }
+
+  /**
+   * Resets the stream.
+   */
+  void reset() {
+    getStream().reset()
+  }
+
+  /**
+   * Gets the result of the command.
+   *
+   * @return String
+   */
+  String getResult() {
+    String result = Parser.stripAwayAnsiCodes(getStream().toString())
+    if (debugMode) {
+      LOGGER.info(result)
     }
+    result
+  }
 
-    /**
-     * Resets the stream.
-     */
-    public void reset() {
-        getStream().reset();
-    }
+  /**
+   * Runs the desired command.
+   *
+   * @param command String
+   * @throws IOException exception
+   */
+  void run(String command) throws IOException {
+    getPos().write((command).getBytes())
+    getPos().write(Config.getLineSeparator().getBytes())
+    getPos().flush()
+    pause()
+  }
 
-    /**
-     * Gets the result of the command.
-     *
-     * @return String
-     */
-    public String getResult() {
-        String result = Parser.stripAwayAnsiCodes(getStream().toString());
-        if (debugMode) {
-            LOGGER.info(result);
-        }
-        return result;
-    }
+  private void pause() {
+    sleep(200)
+  }
 
-    /**
-     * Runs the desired command.
-     *
-     * @param command String
-     * @throws IOException exception
-     */
-    public void run(String command) throws IOException {
-        getPos().write((command).getBytes());
-        getPos().write(Config.getLineSeparator().getBytes());
-        getPos().flush();
-        pause();
-    }
+  private void add(Class<? extends Command>... commands) throws IOException {
 
-    private void pause() {
-        try {
-            Thread.sleep(200);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    registry = new AeshCommandRegistryBuilder().commands(commands).create()
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private void add(Class<? extends Command>... commands) throws IOException {
+    AeshConsoleBuilder consoleBuilder =
+        new AeshConsoleBuilder().settings(settings).commandRegistry(registry)
 
-        registry = new AeshCommandRegistryBuilder().commands(commands).create();
+    aeshConsole = consoleBuilder.create()
+    aeshConsole.start()
+    getStream().flush()
+  }
 
-        AeshConsoleBuilder consoleBuilder =
-            new AeshConsoleBuilder().settings(settings).commandRegistry(registry);
+  void remove(String commandName) {
+    MutableCommandRegistry mutableRegistry = (MutableCommandRegistry) registry
+    mutableRegistry.removeCommand(commandName)
+  }
 
-        aeshConsole = consoleBuilder.create();
-        aeshConsole.start();
-        getStream().flush();
-    }
+  PipedOutputStream getPos() {
+    pos
+  }
 
-    public void remove(String commandName) {
-        MutableCommandRegistry mutableRegistry = (MutableCommandRegistry) registry;
-        mutableRegistry.removeCommand(commandName);
-    }
+  ByteArrayOutputStream getStream() {
+    stream
+  }
 
-    private PipedOutputStream getPos() {
-        return pos;
-    }
+  /**
+   * Gets all registered commands.
+   *
+   * @return Set<String>
+   */
+  Set<String> getRegisteredCommands() {
+    registry.getAllCommandNames()
+  }
 
-    private ByteArrayOutputStream getStream() {
-        return stream;
-    }
+  /**
+   * Gets the current working directory.
+   *
+   * @return String
+   */
+  String getCurrentDirectory() {
+    aeshConsole.getAeshContext().getCurrentWorkingDirectory().getName()
+  }
 
-    /**
-     * Gets all registered commands.
-     *
-     * @return Set<String>
-     */
-    public Set<String> getRegisteredCommands() {
-        return registry.getAllCommandNames();
-    }
-
-    /**
-     * Gets the current working directory.
-     *
-     * @return String
-     */
-    public String getCurrentDirectory() {
-        return aeshConsole.getAeshContext().getCurrentWorkingDirectory().getName();
-    }
-
-    /**
-     * Stops the aesh console.
-     */
-    public void stop() {
-        aeshConsole.stop();
-    }
+  /**
+   * Stops the aesh console.
+   */
+  void stop() {
+    aeshConsole.stop()
+  }
 
 }
