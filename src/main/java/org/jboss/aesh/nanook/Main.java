f8f00895ac5c72024df684c5b4d9196a7018277f
eprogramming <00hf11@gmail.com>
2015-06-18 22:29:04 -0300

    updates aesh,wildfly-swarm,back to java

--- src/main/java/org/jboss/aesh/nanook/Main.java
+++ src/main/java/org/jboss/aesh/nanook/Main.java
@@ -2,7 +2,7 @@
  * JBoss, Home of Professional Open Source Copyright 2014 Red Hat Inc. and/or its affiliates and
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the 'License') you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
@@ -10,28 +10,24 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.nanook
+package org.jboss.aesh.nanook;
 
-import groovy.transform.CompileStatic
-
-import org.wildfly.swarm.container.Container
-import org.wildfly.swarm.container.DefaultWarDeployment
-import org.wildfly.swarm.container.WarDeployment
-import org.wildfly.swarm.undertow.StaticDeployment
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.container.DefaultWarDeployment;
+import org.wildfly.swarm.container.WarDeployment;
 
 /**
  * @author <a href='mailto:00hf11@gmail.com'>Helio Frota</a>
  */
-@CompileStatic
-class Main {
+public class Main {
 
-  static main(String... args) {
+    public static void main(String... args) throws Exception {
 
-    Container container = new Container()
-    WarDeployment deployment = new DefaultWarDeployment(container)
-    deployment.staticContent('/')
-    deployment.getArchive().addClass(AeshHandler)
-    deployment.getArchive().addClass(NanookServlet)
-    container.start().deploy(deployment)
-  }
+        Container container = new Container();
+        WarDeployment deployment = new DefaultWarDeployment(container);
+        deployment.staticContent("/");
+        deployment.getArchive().addClass(AeshHandler.class);
+        deployment.getArchive().addClass(NanookServlet.class);
+        container.start().deploy(deployment);
+    }
 }
